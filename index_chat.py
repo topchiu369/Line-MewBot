4ec005b79fb1f6c15f8973a750d48f583fada941
jazzpujols34 <ro5112@hotmail.com>
2023-05-22 10:30:08 +0800

    Initial commit

---
+++ index_chat.py
@@ -0,0 +1,76 @@
+from flask import Flask, request, abort
+import os
+import openai
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+openai.api_type = "azure"
+openai.api_base = "https://qna-openai.openai.azure.com/"
+openai.api_version = "2023-03-15-preview"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_base = os.getenv("OPENAI_API_BASE")
+
+app = Flask(__name__)
+
+# Initialize messages list with the system message
+messages = [
+    {"role": "system", "content": "You are a helpful assistant that has a deep understanding of cat and also speak like a cat. \
+                                   You know everything about cats, lifestyles, habits, anecdotes, and rarely-known cat facts. \
+                                   You will primarily respond in Traditional Chinese."},
+]
+
+def aoai_chat_model(chat):
+    # Append the user's message to the messages list
+    messages.append({"role": "user", "content": chat})
+
+    response_chat = openai.ChatCompletion.create(
+        engine="gpt-35-turbo",
+        messages=messages,
+        temperature=0.7,
+        max_tokens=800,
+        top_p=0.95,
+        frequency_penalty=0,
+        presence_penalty=0,
+        stop=None
+    )
+
+    # Append the assistant's response to the messages list
+    messages.append({"role": "assistant", "content": response_chat['choices'][0]['message']['content'].strip()})
+
+    return response_chat['choices'][0]['message']['content'].strip()
+
+# while True:
+#     chat = input('來抬槓：')
+#     if chat.lower() == "沒問題了":
+#         break
+#     print(aoai_chat_model(chat))  
+
+line_bot_api = LineBotApi(os.getenv('LINE_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
+
+@app.route("/")
+def saturday():
+    return 'Cat Time!!!'
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler1.handle(body, signature)
+    except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=aoai_chat_model(event.message.text))
+    )
+
+if __name__ == "__main__":
+    app.run()
